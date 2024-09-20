ce229ef376cea05ef707cdd672cf17111a746a20
Ivan Panussis <ivan.panussis.c@gmail.com>
2024-09-19 23:40:30 -0300

    feat(back): Car service created and injected in cars controller

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,19 +1,20 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
-    private cars = ['Toyota','Honda','Jeep'];
+    constructor(
+        private readonly carsService: CarsService
+    ){}
 
     @Get()
     getAllCars() {
-        return this.cars;
+        return this.carsService.findAll();
     }
 
     @Get(':id')
     getCarById( @Param('id') id: string ) {
-        return {
-            cars: this.cars[+id-1]
-        }
+        return this.carsService.findOneById(+id);
     }
 }
