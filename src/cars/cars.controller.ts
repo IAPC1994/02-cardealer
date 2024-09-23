f2bea44998be12f46359391078596702b1631aac
Ivan Panussis <ivan.panussis.c@gmail.com>
2024-09-23 02:24:07 -0300

    feat(back): update and delete methods working

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -26,14 +27,14 @@ export class CarsController {
     
     @Patch(':id')
     updateCar( 
-        @Body() body: any, 
+        @Body() updateCarDto: UpdateCarDto, 
         @Param('id', ParseUUIDPipe) id: string
     ){
-        return { id, body, method: 'PATCH'};
+        return this.carsService.update(id, updateCarDto);
     }
 
     @Delete(':id')
     deleteCar( @Param('id', ParseUUIDPipe) id: string){
-        return {id, method: 'DELETE'};
+        return this.carsService.delete(id);
     }
 }
